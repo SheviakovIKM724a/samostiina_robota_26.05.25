86e276d45fc2645c87f9ba1a70d60ecb8181bd60
Євгеній Шевяков <evgenii.sheviakov@gmail.com>
2025-05-26 09:26:33 +0300

    Завдання №1 (вектори)

--- Самостійна робота (26.05.25).cpp
+++ Самостійна робота (26.05.25).cpp
@@ -1,29 +1,24 @@
 ﻿#include <iostream>
+#include <vector>
+
 using namespace std;
 
-template<typename T>
-bool contains(const T* arr, size_t size, const T& value) {
-    for (size_t i = 0; i < size; ++i) {
-        if (arr[i] == value) {
-            return true;
-        }
-    }
-    return false;
-}
- 
 int main() {
-    system("chcp 1251 > nul"); // Підключення української мови
+    system("chcp 1251 > nul"); // Підключення української мови в консолі
 
-    int arr[] = { 1, 2, 3, 4, 5 };
-    size_t size = sizeof(arr) / sizeof(arr[0]);
+    vector<int> numbers; // створюємо вектор цілих чисел
 
-    int valueToFind = 3;
-    if (contains(arr, size, valueToFind)) {
-        cout << "Значення " << valueToFind << " знайдено в масиві.\n";
+    // Заповнюємо вектор числами від 1 до 10
+    for (int i = 1; i <= 10; ++i) {
+        numbers.push_back(i);
     }
-    else {
-        cout << "Значення " << valueToFind << " не знайдено в масиві.\n";
+
+    // Виводимо елементи вектора на екран
+    cout << "Елементи вектора: ";
+    for (int i = 0; i < numbers.size(); ++i) {
+        cout << numbers[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
